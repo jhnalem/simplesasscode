abb902711139608fd4cecf50538f369f40d0c0c5
yohannes <jhnalem@gmail.com>
2016-02-09 22:03:04 +0000

    added alert  notice for the application

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,24 @@
-class Users::RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController   
+    before_filter :select_plan, only: :new
+
     def create
         super do |resource|
-            if params[:plan]
-                resource.plan_id = params[:plan]
-                if resource.plan_id == 1
-                    resource.save_with_payment
-                else
-                    resource.save
-                end
-            end
+        if params[:plan]
+            resource.plan_id == params[:plan]
+        if resource.plan_id == 1
+            resource.save_with_payment
+        else
+            resource.save
         end
+       end
+     end
+   end
+   private
+  def select_plan
+    unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+      flash[:notice] = "Please select a membership plan to sign up."
+      redirect_to root_url
     end
-end
+
+   end
+   end
