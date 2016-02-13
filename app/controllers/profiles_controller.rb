4458dfd6193061701c9e5a31f7efca55f8337e29
jhnalem <jhnalem@gmail.com>
2016-02-13 23:46:57 +0000

    Intial commit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,9 @@
+####app/controllers/profiles_controller.rb
 class ProfilesController < ApplicationController
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @variable = params[:user_id]
+    @profile = @user.build_profile
+  end
 end
